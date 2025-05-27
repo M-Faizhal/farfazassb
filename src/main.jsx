a097b485575c977369fd9a010a01dbd9cb2d8fa8
bayustwn <khususpby@gmail.com>
2025-05-27 20:42:51 +0700

    feat: adding login logic

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
+import { Toaster } from 'react-hot-toast'
+import { CookiesProvider } from 'react-cookie'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <Toaster/>
+    <CookiesProvider defaultSetOptions={{ path: '/' }}>
+      <App />
+    </CookiesProvider>
   </BrowserRouter>,
 )
