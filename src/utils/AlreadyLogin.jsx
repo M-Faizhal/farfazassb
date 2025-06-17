002229bdb5b6bced2cb4fa3101ea4cf89e4514a2
bayustwn <khususpby@gmail.com>
2025-06-18 06:29:09 +0700

    fix: protectAdmin and AlreadyLogin

--- src/utils/AlreadyLogin.jsx
+++ src/utils/AlreadyLogin.jsx
@@ -1,11 +1,12 @@
-import { jwtDecode } from "jwt-decode";
 import { useCookies } from "react-cookie";
 import { Navigate,Outlet } from "react-router";
 
 const AlreadyLogin = () => {
     const [cookies] = useCookies([import.meta.env.VITE_COOKIES_NAME]);
 
-    return cookies.session?  <Navigate to="/admin/dashboard" replace={true} /> : <Outlet/>;
+    return (cookies.session && cookies.session !== "undefined")
+  ? <Navigate to="/admin/dashboard" replace={true} />
+  : <Outlet />;
 };
 
 export default AlreadyLogin;
--- src/utils/ProtectAdmin.jsx
+++ src/utils/ProtectAdmin.jsx
@@ -1,12 +1,22 @@
 import { jwtDecode } from "jwt-decode";
 import { useCookies } from "react-cookie";
-import { Navigate,Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
 
 const ProtectAdmin = () => {
-    const [cookies] = useCookies([import.meta.env.VITE_COOKIES_NAME]);
-    const isAdmin = cookies.session? jwtDecode(cookies.session)?.role === 'SUPER_ADMIN' || 'COACH' : false;
+  const [cookies] = useCookies([import.meta.env.VITE_COOKIES_NAME]);
 
-  return isAdmin? <Outlet/> : <Navigate to="/admin/login" replace={true} />
+  const isAdmin = (() => {
+    if (!cookies.session || cookies.session === "undefined") return false;
+
+    try {
+      const decoded = jwtDecode(cookies.session);
+      return ['SUPER_ADMIN', 'COACH'].includes(decoded?.role);
+    } catch (error) {
+      return false;
+    }
+  })();
+
+  return isAdmin ? <Outlet /> : <Navigate to="/admin/login" replace />;
 };
 
 export default ProtectAdmin;
